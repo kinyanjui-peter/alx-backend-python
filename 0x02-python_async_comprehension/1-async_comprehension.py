344fb2d69e9ce37328705fa876cc2ecba62db503
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-19 23:30:42 +0300

    Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,6 +9,7 @@ async_generator = __import__('0-async_generator').async_generator
 oroutine called async_comprehension that takes no arguments.
 The coroutine will collect 10 random numbers using an async comprehensing
 over async_generator, then return the 10 random numbers.
+
 """
 
 
