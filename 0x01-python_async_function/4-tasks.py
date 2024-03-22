a7abae7f6404a92458569cb8536e443b0570b52b
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-22 11:29:12 +0300

    function

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,13 +4,23 @@ Take the code from wait_n and alter it into a new function task_wait_n.
 The code is nearly identical to wait_n except task_wait_random is being called.
 """
 # wait_n = __import__("1-concurrent_coroutines").wait_n
+import asyncio
+from typing import List
 task_wait_random = __import__("3-tasks").task_wait_random
 
 
-async def task_wait_n(n, max_delay):
-    resuilts = []
-    # loop wait_random n times
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    tasks = []  # List to store asyncio tasks
+    results = []  # List to store results
+
+    # Create tasks for wait_random
     for _ in range(n):
-        resuilt = await task_wait_random(max_delay)
-        resuilts.append(resuilt)
-    return resuilts
+        task = asyncio.create_task(task_wait_random(max_delay))  # Create task for wait_random
+        tasks.append(task)  # Add task to the list
+
+    # Gather results from completed tasks
+    for task in tasks:
+        result = await task  # Await the task to get the result
+        results.append(result)  # Add result to the list
+
+    return results
