3c67fc5bdba30d77ab405fe1e18fcfd3f59c80d7
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-22 12:42:42 +0300

    Async Comprehensions

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-
+"""
+This coroutine function that will execute async_comprehension four times
+in parallel using asyncio.gather.
+"""
 import asyncio
 import random
-import time
+import datetime import timedelta
+from typing import Generator
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> timedelta:
+    """  A ffunction taht return async comprehension and time"""
     startTime = time.time()
     allTask = await asyncio.gather(async_comprehension(),
                                    async_comprehension(),
